a186a7e23da8e8234a455358c731fbcf60c7780a
maksym.drach <Maksym.Drach@Ciber.com>
2017-04-05 13:35:34 +0200

    Add project files.

---
+++ gitAzureDeploymentTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="gitAzureDeploymentTest.MvcApplication" Language="C#" %>
---
+++ gitAzureDeploymentTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gitAzureDeploymentTest.Startup))]
+namespace gitAzureDeploymentTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
